92aaae6498d24276ec0ea6aea3aca7e2d80be772
dummy <divyanshu.kumar.backup@gmail.com>
2026-03-26 21:53:00 +0530

    [Auth] added auth from jwt header

--- apps/web/src/lib/supabase/server.ts
+++ apps/web/src/lib/supabase/server.ts
@@ -1,5 +1,6 @@
 import { createServerClient } from "@supabase/ssr";
 import { cookies } from "next/headers";
+import { createClient as createSupabaseClient } from '@supabase/supabase-js'
 
 export async function createClient() {
   const cookieStore = await cookies();
@@ -24,4 +25,14 @@ export async function createClient() {
       },
     }
   );
+}
+
+
+export  function createClientFromToken(token : string) {
+  return createSupabaseClient(
+      process.env.NEXT_PUBLIC_SUPABASE_URL!,
+      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+      { global: { headers: { Authorization: `Bearer ${token}` } } }
+    )
+
 }
